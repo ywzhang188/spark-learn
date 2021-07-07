83fbc94ca83ca64eb49906acb7ffdfc3dffaefeb
ywzhang188 <ywzhang188@gmail.com>
2021-07-07 11:08:49 +0800

    add spark session

--- getting_started/spark_session.py
+++ getting_started/spark_session.py
@@ -16,3 +16,20 @@ local_spark_example_dir = "file://{}/".format("/root/apps/spark-2.4.4-bin-hadoop
 spark = SparkSession.builder.appName("Read Parquet Data").config("spark.some.config.option", "some-value").getOrCreate()
 
 sc = spark.sparkContext
+
+
+#
+spark = (SparkSession
+         .builder
+         .appName("test-dockerlinuxcontainer")
+         .enableHiveSupport()
+         .config("spark.executor.instances", "50")
+         .config("spark.executor.memory","48g")
+         .config("spark.executor.cores","24")
+         .config("spark.driver.memory","48g")
+         .config("spark.sql.shuffle.partitions","500")
+         .config("spark.yarn.appMasterEnv.yarn.nodemanager.container-executor.class","DockerLinuxContainer")
+         .config("spark.executorEnv.yarn.nodemanager.container-executor.class","DockerLinuxContainer")
+         .config("spark.yarn.appMasterEnv.yarn.nodemanager.docker-container-executor.image-name","bdp-docker.jd.com:5000/wise_mart_bag:latest")
+         .config("spark.executorEnv.yarn.nodemanager.docker-container-executor.image-name","bdp-docker.jd.com:5000/wise_mart_bag:latest")
+         .getOrCreate())
