eb15fbdd81a0f0fd140a4c0845c10e9349afc8af
ywzhang188 <ywzhang188@gmail.com>
2021-08-27 20:23:21 +0800

    udf count row wise count NONE

--- spark_dataframe/dataframe_udf.py
+++ spark_dataframe/dataframe_udf.py
@@ -86,3 +86,8 @@ def jieba_f(line):
         return []
 jieba_udf = udf(jieba_f, ArrayType(StringType()))
 douban_df = douban_df.withColumn('Words', jieba_udf(col('Comment')))
+
+# row wise count None
+df = sqlContext.createDataFrame([(None, None), (1, None), (None, 2)], ("a", "b"))
+count_empty_columns = udf(lambda row: len([x for x in row if x == None]), IntegerType())
+new_df = df.withColumn("null_count", count_empty_columns(struct([df[x] for x in df.columns])))
